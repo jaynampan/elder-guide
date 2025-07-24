92d27f1a7addf3bfc7f7fd432790754eaf3a1209
JaynamPan <jaynamwang@outlook.com>
2025-07-24 09:44:05 +0800

    setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 pluginManagement {
     repositories {
+        // Use AliYun mirrors if you have internet issues
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        // End of mirrors
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +20,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Use AliYun mirrors if you have internet issues
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        // End of mirrors
         google()
         mavenCentral()
     }
